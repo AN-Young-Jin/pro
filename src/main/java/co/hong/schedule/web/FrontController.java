8a90ed1748aa4a5f54e759f6a975ba9d8b92553e
admin <admin@DESKTOP-P9ECKF8>
2023-07-07 11:06:02 +0900

    홍규연 1105

--- src/main/java/co/hong/schedule/web/FrontController.java
+++ src/main/java/co/hong/schedule/web/FrontController.java
@@ -26,6 +26,7 @@ public class FrontController extends HttpServlet {
 
 	public void init(ServletConfig config) throws ServletException {
 		map.put("/mainControl.do", new mainControl());
+		map.put("fdva", fdsasg())
 	}
 	
 	
